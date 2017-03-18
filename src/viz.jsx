9442d5bd87990adbdddf31debc7235d2d8f9d62d
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2017-03-18 18:56:18 -0400

    figure out transition animations

--- src/viz.jsx
+++ src/viz.jsx
@@ -1,7 +1,8 @@
 const React = window.React;
 const { Map, TileLayer } = window.ReactLeaflet;
+const d3 = window.d3;
 
-const Line = require('rc-progress').Line;
+// const Line = require('rc-progress').Line;
 
 /* macro object containing all the state */
 class Visualization extends React.Component {
@@ -10,30 +11,43 @@ class Visualization extends React.Component {
         this.state = {
             zoom: 13,
             bounds: [[40.6794268, -73.92989109999999], [40.789747, -74.075979]],
-            scroll_ticks: 50,
-            max_scroll_ticks: 100,
+            scroll_ticks: 0,
+            max_scroll_ticks: 100
         };
 
-        this.handleScroll = this.handleScroll.bind(this);  // binding is necessary to make "this" work in the callback
+        // binding is necessary to make "this" work
+        this.handleScroll = this.handleScroll.bind(this);
+        this.setScreenState = this.setScreenState.bind(this);
+
+        this.setScreenState();
     }
 
+    /* the main event loop for the visualization */
     handleScroll(e) {
         if (e.deltaY > 0) {
-            console.log("Scrolled down...");
             if (this.state.scroll_ticks < this.state.max_scroll_ticks) {
                 this.state.scroll_ticks += 1;
+                this.setState({newForm: true});
             }
         } else {
-            console.log("Scrolled up...");
             if (this.state.scroll_ticks > 0) {
                 this.state.scroll_ticks--;
+                this.setState({newForm: true});
             }
         }
-        console.log(this.state.scroll_ticks);
+    }
+
+    setScreenState() {
+        let percent = this.state.scroll_ticks / this.state.max_scroll_ticks * 100;
+        let s = this.state.screen_state = {};
+
+        s.intro = (percent < 5);
     }
 
 
     render() {
+        let percent = this.state.scroll_ticks / this.state.max_scroll_ticks * 100;
+
         return (
             <div className="visualization-content-frame" onWheel={this.handleScroll}>
                 {[
@@ -45,8 +59,10 @@ class Visualization extends React.Component {
                         subdomains='abcd'
                         key={1}>
                     </NYCMap>,
-                    <Overlay key={2}/>,
-                    <Scrollbar percent={this.state.scroll_ticks} key={3}/>
+                    <Overlay key={2}
+                             percent={percent}
+                             />,
+                    <Scrollbar percent={percent} key={3}/>
                 ]}
             </div>
         );
@@ -71,16 +87,35 @@ class NYCMap extends React.Component {
 
 /* display element superimposed on the map that is vertically and horizontally anchored in the center of the screen */
 class Overlay extends React.Component {
+    constructor() {
+        super();
+        this.state = {introTextOnScreen: false}
+    }
+
     render() {
-        return <div className="overlay">
-            <IntroScreen/>
-        </div>;
+        if (this.state.introTextOnScreen) {
+            this.state.introTextOnScreen = false;
+            return <div className="overlay"><IntroScreen fadeIn={false} fadeOut={true}/></div>
+        }
+        else {
+            this.state.introTextOnScreen = true;
+            return <div className="overlay"><IntroScreen fadeIn={true} fadeOut={false}/></div>
+        }
+    }
+
+    /* only redraw if we need to transition */
+    shouldComponentUpdate(nextProps, nextState) {
+        return ((nextProps.percent <= 5 && this.state.introTextOnScreen == false) ||
+                (nextProps.percent > 5 && this.state.introTextOnScreen == true));
     }
 }
 
 class IntroScreen extends React.Component {
     render() {
-        return <div className="intro-screen-frame">
+        let transition_class = this.props.fadeIn ? "show" : "hide";
+        let class_name = "intro-screen-frame " + transition_class;
+
+        return <div className={class_name}>
             <div className="intro-screen-content">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse est purus, rhoncus id nulla vel, rhoncus ullamcorper risus. Aenean nulla arcu, dignissim vel metus in, luctus convallis odio. Etiam eget ipsum nec lacus ultrices efficitur quis non nibh. Phasellus ullamcorper risus et ex aliquam mollis. Ut fringilla a tellus at condimentum. In ut fringilla elit, ac placerat ipsum. Aliquam congue ac mauris non vehicula. Aenean vitae nulla in elit semper venenatis. Duis auctor eros ante. Curabitur cursus odio risus, et euismod elit fermentum quis. Donec fermentum odio libero, eget vulputate risus accumsan eu. Aliquam tristique nibh fermentum ligula tempor porttitor. Pellentesque convallis tempor lectus, in euismod nunc eleifend ac. Duis faucibus ultricies est nec ornare. Nam pulvinar ultricies lorem, hendrerit varius urna aliquam vitae.
             </div>
@@ -94,14 +129,7 @@ class IntroScreen extends React.Component {
 
 class Scrollbar extends React.Component {
     render() {
-        console.log("RERENDERING...");
-        return <div className="scroll-bar">
-            <Line percent={this.props.percent}
-                  strokeWidth="1"
-                  strokeColor="gray"
-                  strokeLinecap="square"
-                  trailColor="white"/>
-        </div>
+        return (<div className="scroll-bar" style={{width: this.props.percent + "%"}}></div>);
     }
 }
 
